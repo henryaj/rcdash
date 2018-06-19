77338a10d39d1c048fbbb2acf244b75f5964e049
Henry Stanley <henry@henrystanley.com>
2018-06-18 21:42:38 -0400

    be slightly less verbose

--- sniffer.rb
+++ sniffer.rb
@@ -5,7 +5,6 @@ class Sniffer
     PTY.spawn( "tshark -Tfields -e eth.addr" ) do |stdout, _, _|
       begin
         stdout.each do |line|
-          puts "hi"
           next if line.include?("Capturing")
           mac = line.split(",").last.strip
           record_user_seen(mac)
@@ -19,8 +18,9 @@ class Sniffer
   def self.record_user_seen(mac)
     u = User.where(mac: mac)
     if u.any?
-      puts "boom"
-      u.first.seen!
+      user = u.first
+      puts "--- [#{Time.now}] Saw #{user.name}"
+      user.seen!
     end
   end
 end
