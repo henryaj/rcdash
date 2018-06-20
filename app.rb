2d8bba6cab10ddf0be388b70b30c7a8ec6efbbc9
Henry Stanley <henry@henrystanley.com>
2018-06-20 16:32:26 -0400

    idempotently create users table

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sequel'
 require 'pg'
 DB = Sequel.connect(ENV.fetch("DATABASE_URL"))
 
+unless DB.table_exists?(:users)
+  DB.create_table :users do
+    primary_key :id
+    String :mac
+    String :email
+    String :zulip_id
+    String :name
+    DateTime :last_seen
+  end
+end
+
 require './user'
 require './botserver'
